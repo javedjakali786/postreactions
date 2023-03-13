92b651d0191001455e5cecc5bbd22a937dd2d1d9
javedjakali786 <44975340+javedjakali786@users.noreply.github.com>
2023-03-14 02:30:03 +0530

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -2,5 +2,5 @@ api_id = 25983686
 api_hash = "d49ffa3e2b617c66250b7f4c169d1cb9"
 chats = ['chatkatana', 'nevzorovtv', 'chvkredantg', 'topor', 'jolynews']
 chance = 0
-reactions = '💩'
+reactions = '❤️'
 ignorelist = []
